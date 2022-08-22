289e7dd8543cb4fb3fa6b24639f395991ec95fd8
Lukas Traue <lukas.traue@deutschebahn.com>
2022-08-22 16:27:16 +0200

    group anagrams

--- ArraysAndHashing/group_anagrams.js
+++ ArraysAndHashing/group_anagrams.js
@@ -1,7 +1,15 @@
-const isAnagram = (w1, w2) => {
-    
+var groupAnagrams = function(strs) {
+    var anagramDict = {};
+    strs.forEach(element => {
+        let key = element.split("").sort().join("");
+        if (!anagramDict[key]) {
+            anagramDict[key] = [element]
+        } else {
+            anagramDict[key].push(element);
+        }
+    });
+    return Object.values(anagramDict);
 };
 
-var groupAnagrams = function(strs) {
-    
-};
+strs = ["eat","tea","tan","ate","nat","bat"]
+console.log(groupAnagrams(strs));
